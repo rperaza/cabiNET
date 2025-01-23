7c6e47febc77e5450271a1f65c91b35f33072501
rperaza <9965804+rperaza@users.noreply.github.com>
2025-01-23 06:28:49 -0600

    Modified navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,29 +4,27 @@ import logo from '/logo.png';
 
 const Navbar = () => {
     return (
-        <>
-            <div className="global-navbar">
-                <div className="navbar-logo">
-                    <a href="#home"><img src={logo} alt="logo" className="navbar-logo" title="home" /></a>
+        <div className="global-navbar">
+            <div className="navbar-logo">
+                <a href="#home"><img src={logo} alt="logo" className="navbar-logo" title="home" /></a>
+            </div>
+            <div className="navbar-search-container">
+            <form class="search-bar-form">
+                <div class="search-bar">
+                    <input type="text" placeholder="Search.." name="search" />
+                    <button type="submit"><i class="fa fa-search"></i></button>
                 </div>
-                <div className="navbar-search-container">
-                <form class="search-bar-form">
-                    <div class="search-bar">
-                        <input type="text" placeholder="Search.." name="search" />
-                        <button type="submit"><i class="fa fa-search"></i></button>
-                    </div>
-                </form>
+            </form>
+            </div>
+            <div className="right-user">
+                <div className="user-avatar">
+                    <img src="https://www.w3schools.com/howto/img_avatar.png" alt="avatar" className="avatar"/>
                 </div>
-                <div className="right-user">
-                    <div className="user-avatar">
-                        <img src="https://www.w3schools.com/howto/img_avatar.png" alt="avatar" className="avatar"/>
-                    </div>
-                    <div className="user-name">
-                        <p>John Doe</p>
-                    </div>
+                <div className="user-name">
+                    <p>John Doe</p>
                 </div>
             </div>
-        </>
+        </div>
     )};
 
     export default Navbar;
